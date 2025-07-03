aebcf92636295fdfe4dbed7a1baa2a0657a3eb9a
Leandro Oliveira <leandro.gooli@gmail.com>
2025-07-03 17:45:30 +0100

    Update Next.js config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,21 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/dashboard/create',
+        permanent: false,
+        has: [
+          {
+            type: 'cookie',
+            key: 'sb-access-token',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
