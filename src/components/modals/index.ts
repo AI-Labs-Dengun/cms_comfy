10e0a3e9d565ad29ced100bac8884e976fe8fc41
Leandro Oliveira <leandro.gooli@gmail.com>
2025-10-02 10:52:02 +0100

    Remove NotificationModal component

--- src/components/modals/index.ts
+++ src/components/modals/index.ts
@@ -1,3 +1,2 @@
 export { default as DeleteConfirmationModal } from './DeleteConfirmationModal';
 export { default as PublishToggleModal } from './PublishToggleModal';
-export { default as NotificationModal } from './NotificationModal'; 
